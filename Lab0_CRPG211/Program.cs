179041075c58e06db802844ab21007ebce0f4c06
Frost <152447084+Frostwolf74@users.noreply.github.com>
2025-01-29 18:33:40 -0700

    added file writer, prime number finder and input verification. Completed assignment

--- Lab0_CRPG211/Program.cs
+++ Lab0_CRPG211/Program.cs
@@ -1,40 +1,94 @@
 ﻿namespace Lab0_CRPG211;
 
-/*
-Open the Visual Studio IDE.
-    Create a new C# Console App project.
-    Open the Program.cs file.
-    Prompt the user to enter the low number.
-    Store the low number as an int variable.
-    Prompt the user to enter the high number.
-    Store the high number as an int variable.
-    Calculate and print out the difference between the low and high variables.
-    Task 2: Looping and Input Validation
-Write a loop that keeps iterating until the user enters a positive low number.
-    Write a loop that keeps iterating until the enters a high number greater than the low number.
-    Task 3: Using Arrays and File I/O
-Create an array variable that holds every number between low and high.
-    Create a new file called "numbers.txt".
-Write each number in the array to the file in reverse order (largest to smallest).
-Additional Tasks
-Use methods get and validate the users input.
-Read the numbers back from the file and print out the sum of the numbers.
-Use a List instead of an array variable to store the numbers.
-Use the double data type instead of the int data type.
-Print out the prime numbers between low and high.
-Write a method that takes in a number and returns true if it is a prime number.
-You may need to use the modulus operator.*/
-
 class Program{
     static void Main(string[] args){
         Console.WriteLine("Enter the low number: ");
-        int lowNum = int.Parse(Console.ReadLine());
-        
+        double lowNum = double.Parse(Console.ReadLine());
+
         Console.WriteLine("Enter the high number: ");
-        int highNum = int.Parse(Console.ReadLine());
+        double highNum = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Difference: " + (highNum - lowNum));
+
+        double num = highNum;
+        while (ValidateInput(num, highNum)){
+            Console.WriteLine("Enter low number: ");
+            num = double.Parse(Console.ReadLine());
+        }
+
+        double num1 = lowNum;
+        while (ValidateInput(num1, lowNum, highNum)){
+            Console.WriteLine("Enter high number: ");
+            num1 = double.Parse(Console.ReadLine());
+        }
+
+        // int[] a = new int[highNum - lowNum + 1];
+        List<double> l = new List<double>(); // list implementation
         
-        Console.WriteLine(highNum - lowNum);
+        for (double i = lowNum; i <= highNum; ++i){
+            // a[i - lowNum] = i;
+            l.Add(i);
+        }
+
+        using (StreamWriter sw = File.CreateText("./numbers.txt")){
+            // for (int i = a[a.Length - 1]; i >= a[0]; --i){
+            //     sw.WriteLine(i);
+            // }
+
+            for (double i = l[l.Count - 1]; i >= l[0]; --i){
+                sw.WriteLine(i);
+            }
         
+            sw.Close();
+        }
         
+        List<double> primeNumbers = findPrimeNumbers(lowNum, highNum);
+        
+        Console.WriteLine("Prime numbers: ");
+        foreach(double i in primeNumbers){
+            Console.Write(i + ", ");   
+        }
+        
+        Console.WriteLine("\nEnter any number: ");
+        if (verifyPrimeNumber(double.Parse(Console.ReadLine()))){
+            Console.WriteLine("Number is prime");
+        }
+        else{
+            Console.WriteLine("Number is not prime");
+        }
+    }
+
+    public static List<double> findPrimeNumbers(double lowNum, double highNum){
+        List<double> primeNumbers = new List<double>();
+        
+        for(double i = lowNum; i < highNum; ++i) {
+            if (verifyPrimeNumber(i)){
+                primeNumbers.Add(i);
+            }
+        }
+        
+        return primeNumbers;
+    }
+
+    public static Boolean verifyPrimeNumber(double num){
+        if (num < 2) return false;
+        if (num % 2 == 0) return false; // weed out even numbers, guaranteed not prime
+        
+        for (int i = 2; i <= 101; ++i){ // this range accounts for all prime numbers
+            if (num % i != 0){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    public static Boolean ValidateInput(double num, double highNum){
+        return !(num >= highNum || num < 0);
+    }
+
+    public static Boolean ValidateInput(double num, double lowNum, double highNum){ // overload for different uses
+        return !(num <= lowNum || num > highNum);
     }
 }
+    
